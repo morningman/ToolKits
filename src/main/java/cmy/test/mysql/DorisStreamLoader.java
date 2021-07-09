cebdd6b931803ac75b6f6162d09bf460273da7b5
morningman <morningman@163.com>
2021-07-09 10:06:10 +0800

    Modify Stream Loader

--- src/main/java/cmy/test/mysql/DorisStreamLoader.java
+++ src/main/java/cmy/test/mysql/DorisStreamLoader.java
@@ -19,32 +19,71 @@ package cmy.test.mysql;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpHeaders;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.FileEntity;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
 public class DorisStreamLoader {
     // 1. 对于公有云公户，这里填写 Compute Node 地址以及 HTTP 协议访问端口（8040）。
     // 2. 对于开源用户，可以选择填写 FE 地址以及 FE 的 http_port，但须保证客户端和 BE 节点的连通性。
-    private final static String HOST = "your_host";
-    private final static int PORT = 8040;
+    private final static String HOST = "127.0.0.1";
+    private final static int PORT = 9030;
     private final static String DATABASE = "db1";   // 要导入的数据库
     private final static String TABLE = "tbl1";     // 要导入的表
     private final static String USER = "root";      // Doris 用户名
-    private final static String PASSWD = "";        // Doris 密码
-    private final static String LOAD_FILE_NAME = "/path/to/1.txt"; // 要导入的本地文件路径
+    private final static String PASSWD = "12345";        // Doris 密码
+    private final static String LOAD_FILE_NAME = "/Users/chenmingyu/Downloads/log/1.txt"; // 要导入的本地文件路径
 
     private final static String loadUrl = String.format("http://%s:%s/api/%s/%s/_stream_load",
             HOST, PORT, DATABASE, TABLE);
+
+    private final static HttpClientBuilder httpClientBuilder2 = HttpClients
+            .custom()
+            .setRedirectStrategy(new DefaultRedirectStrategy() {
+
+                @Override
+                public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context) {
+                    return true;
+                }
+
+                public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context) throws ProtocolException {
+                    URI uri = this.getLocationURI(request, response, context);
+                    String method = request.getRequestLine().getMethod();
+                    if (method.equalsIgnoreCase("HEAD")) {
+                        return new HttpHead(uri);
+                    } else if (method.equalsIgnoreCase("GET")) {
+                        return new HttpGet(uri);
+                    } else {
+                        int status = response.getStatusLine().getStatusCode();
+                        HttpUriRequest toReturn = null;
+                        if (status == 307 || status == 308) {
+                            toReturn = RequestBuilder.copy(request).setUri(uri).build();
+                            toReturn.removeHeaders("Content-Length"); //Workaround for an apparent bug in HttpClient
+                        } else {
+                            toReturn = new HttpGet(uri);
+                        }
+                        return toReturn;
+                    }
+                }
+            });
 
     private final static HttpClientBuilder httpClientBuilder = HttpClients
             .custom()
@@ -68,7 +107,9 @@ public class DorisStreamLoader {
 
             // 设置导入文件。
             // 这里也可以使用 StringEntity 来传输任意数据。
-            FileEntity entity = new FileEntity(file);
+            // FileEntity entity = new FileEntity(file);
+            String d = "1,2\n3,4";
+            StringEntity entity = new StringEntity(d);
             put.setEntity(entity);
 
             try (CloseableHttpResponse response = client.execute(put)) {
@@ -93,7 +134,7 @@ public class DorisStreamLoader {
         byte[] encoded = Base64.encodeBase64(tobeEncode.getBytes(StandardCharsets.UTF_8));
         return "Basic " + new String(encoded);
     }
-    
+
     public static void main(String[] args) throws Exception {
         DorisStreamLoader loader = new DorisStreamLoader();
         File file = new File(LOAD_FILE_NAME);
